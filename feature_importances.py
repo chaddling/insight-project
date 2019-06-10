c651b03e9e71010c566b00bfa7f202a544352b9b
chaddling <chad.m.gu@gmail.com>
2019-06-09 23:27:45 -0400

    eda notebooks

--- feature_importances.py
+++ feature_importances.py
@@ -3,6 +3,9 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
+# compute the feature importances using the default implementation in sklearn's random forest.
+# the default implementation uses gini importance averaged over all trees
+
 # feature ames (used for plotting)
 names = ["# prerelease updates", "# post-release updates", "mean update time", "game description",
          "# of movies", "# of images", "action", "adventure", "casual", "mmo", "racing", "rpg",
@@ -19,17 +22,34 @@ def get_feature_names(indices, feature_names):
 with open('pickle_rf_model.pkl', 'rb') as file:
     rf = pickle.load(file)
 
+print(rf[1])
+
+# load scaler
+with open('pickle_scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
 data = pd.read_csv('data/features_vec_all.csv') # this does not include the no review ones
-X = data[feature_names].values
+
+X = data[feature_names]
+
+# log transform on the data and rescale
+X['num_pre'] = np.log(1 + 100*X['num_pre'])
+X['num_post'] = np.log(1 + 200*X['num_post'])
+X['mean_time'] = np.log(1 + 200*X['mean_time'])
+X['len_description'] = np.log(1+X['len_description'])
+
+X = X.values
 Y = data['sentiment'].values
+
+scaler.fit_transform(X[:,0:5])
 
 # fit the model using the whole dataset
 # with that we'll calculate the feature importances
-rf.fit(X, Y)
+rf[1].fit(X, Y)
 
 # calculated over all leaves
-importances = rf.feature_importances_
-std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+importances = rf[1].feature_importances_
+std = np.std([tree.feature_importances_ for tree in rf[1].estimators_], axis=0)
 
 indices = np.argsort(importances)[::-1]
 
@@ -40,7 +60,7 @@ for f in range(X.shape[1]):
 
 # Plot the feature importances of the forest
 plt.figure()
-plt.title("Feature importances", fontsize=16)
+plt.ylabel("Feature importances", fontsize=16)
 plt.bar(range(X.shape[1]), importances[indices],
        color="r", yerr=std[indices], align="center")
 plt.xticks(range(X.shape[1]), get_feature_names(indices, names),rotation='vertical', fontsize=14)
--- train_rf_classifier.py
+++ train_rf_classifier.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import pickle
 
-from collections import Counter
 from sklearn.preprocessing import label_binarize, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
+
+from plot_roc import plot_roc_curve
 
 # traing the random forest classifier
 
@@ -20,16 +21,28 @@ feature_names = ['num_pre', 'num_post', 'mean_time', 'len_description',
        'is_casual', 'is_mmo', 'is_racing', 'is_rpg', 'is_simulation',
        'is_sports', 'is_strategy']
 
-X = data[feature_names].values
+X = data[feature_names]
+
+# log transform on the data and rescale
+X['num_pre'] = np.log(1 + 100*X['num_pre'])
+X['num_post'] = np.log(1 + 200*X['num_post'])
+X['mean_time'] = np.log(1 + 200*X['mean_time'])
+X['len_description'] = np.log(1+X['len_description'])
+
+X = X.values
 Y = data['sentiment'].values
 
+# rescale only the continuous variables by (x- mu) / std
 scaler = StandardScaler(copy=False)
 scaler.fit_transform(X[:,0:5])
 
 #Y = label_binarize(Y, classes=[-1,0,1])
 
-# Y_hold Counter({1.0: 1710, 0.0: 613, -1.0: 88}  for n_reviews > 50
-X_train, X_hold, Y_train, Y_hold = train_test_split(X, Y, test_size=0.4)
+# split the data:
+# 1. training data is then further over sampled in the pipeline then split up
+# during hyperparametertuning/CV
+# 2. test data will preserve the class proportion in the original data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, test_size=0.2)
 
 sm = SMOTE()
 rf = RandomForestClassifier()
@@ -40,8 +53,8 @@ pipeline = Pipeline([('sm', sm), ('rf', rf)])
 
 kf = StratifiedKFold(n_splits=5)
 
-params = {'rf__n_estimators': list(range(5,100)),
-          'rf__max_depth': list(range(5,50))}
+params = {'rf__n_estimators': list(range(5,20)),
+          'rf__max_depth': list(range(5,20))}
 
 grid = GridSearchCV(estimator=pipeline,
                     param_grid=params,
@@ -49,12 +62,30 @@ grid = GridSearchCV(estimator=pipeline,
                     cv=kf,
                     n_jobs=2)
 
-# after hyperparameter optimization, fit on the held out data
-grid.fit(X_hold, Y_hold)
+grid.fit(X_train, Y_train)
+
+
+print(grid.best_estimator_)
+print('best score = ', grid.best_score_)
+cv_results = grid.cv_results_
+
+print('mean_score +/- std =', cv_results['mean_test_score'].mean(), cv_results['std_test_score'].mean())
+
+
+Y_score = grid.predict_proba(X_test)
+
+# one-hot encode the classes
+Y_test = label_binarize(Y_test, classes=[-1,0,1])
+
+print(Y_score.shape, Y_test.shape)
+
+# evaluate the model on AUC score on the test data
+plot_roc_curve(3, Y_test, Y_score)
+
 
 pkl_filename = "pickle_rf_model.pkl"
 with open(pkl_filename, 'wb') as file:
-    pickle.dump(rf, file)
+    pickle.dump(grid.best_estimator_, file)
 
 scl_filename = "pickle_scaler.pkl"
 with open(scl_filename, 'wb') as file:
